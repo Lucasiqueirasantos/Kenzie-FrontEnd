4bb5870f52359a9721df2e40a8778fd920a45a2a
Lucasiqueirasantos <lucasiqueirasantos@hotmail.com>
2022-02-22 19:19:20 +0000

    Add entrega S2B36

---
+++ EntregaS2B36/snapCrackle.js
@@ -0,0 +1,12 @@
+function snapCrackle () {
+
+    for (i = 1; i <= snapCrackle.length; i++) {
+        if (i = 2 && (i % 2) == 0);
+        console.log ("Snap");
+        if (i = 1 && (i % 5) == 0);
+        console.log ("Crackle");
+        if ((i = 2 && (i % 2) == 0) && (i = 1 && (i % 5) == 0));
+        console.log ("SnapCrackle");
+    }
+    console.log (snapCrackle (maxValue));
+}
